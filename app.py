1c8e743e7faa1f780c106c181b88236743055df2
anilradar <anilradar099@gmail.com>
2024-07-03 16:28:31 +0530

    added mandal attribute

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 country = "India"
 state = "Telangana"
 district = "Karimnagar"
+mandal = "Jammikunta"
 
 print("My country is: ", country)
 print("My state is: ", state)
 print("My district is: ", district)
+print("My mandal is: ", mandal)
