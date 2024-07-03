0fdc93ba038f05a455aa9f7015017a835cb20840
anilradar <anilradar099@gmail.com>
2024-07-03 17:11:07 +0530

    added house no.

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ country = "India"
 state = "Telangana"
 district = "Karimnagar"
 mandal = "Jammikunta"
-village = "Sirisedeu"
+village = "Sirisedu"
+house = "1-22"
 
 print("My country is: ", country)
 print("My state is: ", state)
 print("My district is: ", district)
 print("My mandal is: ", mandal)
 print("My village is: ", village)
+print("My house number is: ", house)
