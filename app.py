8252e272cf5a2a1be4a369ad57307e3c64b912eb
anilradar <anilradar099@gmail.com>
2024-07-03 16:38:46 +0530

    added village attribute

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ country = "India"
 state = "Telangana"
 district = "Karimnagar"
 mandal = "Jammikunta"
+village = "Sirisedeu"
 
 print("My country is: ", country)
 print("My state is: ", state)
 print("My district is: ", district)
 print("My mandal is: ", mandal)
+print("My village is: ", village)
