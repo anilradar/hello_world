1d69d83e96bb23d5c954a4b7fa458e28d7abe688
anilradar <anilradar099@gmail.com>
2024-07-03 16:54:01 +0530

    Removed mandal attribute

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 country = "India"
 state = "Telangana"
 district = "Karimnagar"
-mandal = "Jammikunta"
+# mandal = "Jammikunta"
 # village = "Sirisedeu"
 
 print("My country is: ", country)
 print("My state is: ", state)
 print("My district is: ", district)
-print("My mandal is: ", mandal)
+# print("My mandal is: ", mandal)
 # print("My village is: ", village)
